00114c52523300867ad792c9733fc84cc18ba6d0
Ryan K <136540593+scibee8@users.noreply.github.com>
2024-04-23 17:29:37 -0400

    reversed changes

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ window.onload = () => {
             });
             
             entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude,
+                latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
             document.querySelector("a-scene").appendChild(entity);
